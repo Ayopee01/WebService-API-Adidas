66858c6e2998ba1f88310c44f4fc0c31263e2023
Ayopee01 <Ayopee001@gmail.com>
2025-06-24 16:12:29 +0700

    add res.send

--- Backend/server.js
+++ Backend/server.js
@@ -12,3 +12,7 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('API Adidas Service Running');
+});
